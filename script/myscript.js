aeb1ee252e41d91f615b85f380f02d13b40d6410
jktr3025 <jktr3025@naver.com>
2024-03-05 22:29:37 +0900

    luminous day1

---
+++ script/myscript.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    //banner
+    $(".close1").click(function(){
+        $(".banner").slideUp();
+    })
+    $(".b_2 span").click(function(){
+        $(this).toggleClass("plus")
+    })
+
+})//jquery
